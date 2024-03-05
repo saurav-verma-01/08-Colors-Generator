98e6ba1c78618f270d6149b4335a1913c84d7679
Saurav kumar verma <sauravkumarverma22@gmail.com>
2024-03-06 00:48:43 +0530

    copy to clipboard function

--- src/components/ColorBlock.jsx
+++ src/components/ColorBlock.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { toast } from "react-toastify";
 
 const ColorBlock = ({ color }) => {
   const { hex } = color;
+  const saveToClipboard = async () => {
+    if (navigator.clipboard) {
+      try {
+        await navigator.clipboard.writeText(`#${hex}`);
+        toast.success("Color Copied to clipboard");
+      } catch (error) {
+        toast.error("Failed to copy color to Clipboard");
+      }
+    } else {
+    }
+  };
   return (
     <div
+      onClick={saveToClipboard}
       className="color-block"
       style={{ background: `#${hex}` }}
     >{`#${hex.toUpperCase()}`}</div>
